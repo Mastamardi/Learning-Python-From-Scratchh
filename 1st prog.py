78fe1f3e1aa6f14c41f62a5b1c9fde1fe8891729
OMKAR <omkarbabumastamardi@gmail.com>
2024-12-09 22:50:39 +0530

    Learning Pthon

---
+++ 1st prog.py
@@ -0,0 +1,131 @@
+
+# name = "OMKAR"
+# age = 20
+# price = 99.9
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# a=24
+# b=32
+# sum=a+b
+
+# print(sum)
+
+
+# a=24
+# b=32
+# sum=a-b
+
+# print(sum)
+
+# types of operators
+#Arithmetic Operator
+# a=12
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# print(a%b)
+
+# a=12
+# b=5
+# sum=a+b
+# print(sum)
+
+#->Relational Operators <-
+
+# a=17
+# b=7
+# print(a==b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+
+#->Assignment Operators <-
+
+# num=10
+# num=num+10
+# print(num)
+
+# num=10
+# num+=10
+# print(num)
+
+# num=10
+# num*=10
+# print(num)
+
+# num=10
+# num**=10
+# print(num)
+
+# -> Logical Operator <-
+#not operator
+# a=17
+# b=7
+# print(not False)
+# print(not (a>b))
+
+#and operator
+# val1=True
+# val2=False
+# print("The Final Operator is:",val1 and val2)
+
+#or operator
+# val1=False
+# val2=False
+# print("The Final Operator is:",val1 or val2)
+
+#type conversion
+
+# a=12.5
+# b=int("5")
+# print(type(b))
+# print(a+b)
+
+
+# -> Inputs in Python
+
+# name=input("Enter your Name:")
+# print("Welcome",name)
+
+# val = input("Enter any thing:")
+# print(type(val),val)
+
+
+# name= (input("Enter Your Name:"))
+# age=int(input("Enter Your Age:"))
+# marks=float(input("Enter Your Marks:"))
+
+# print("hello these are the details that you have entered:",name,age,marks)
+
+# -> practice question 1
+# a=float(input("Enter the First Number:"))
+# b=float(input("Enter the Second Number:"))
+
+# print("The Final Sum Of two Numbers is:",a+b)
+
+# -> practice question 2
+
+# a=float(input("Enter one side of the square:"))
+# print("Total Area =",a*a)
+
+# -> practice question 3
+
+# a=float(input("Enter the First Number:"))
+# b=float(input("Enter the Second Number:"))
+# print("Average of these 2 Numbers:",(a+b)/2)
+
+# -> practice question 4
+
+a=float(input("Emter the First Number:"))
+b=float(input("Enter the Second Number:"))
+
+print(a>=b)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+a=17
+b=7
+print(not False)
+print(not (a>b))
