48ef4d724c95606f6c6f5b0f5fd6e122415070f2
OMKAR <omkarbabumastamardi@gmail.com>
2024-12-11 16:09:46 +0530

    added 4th day

---
+++ 4th day .py
@@ -0,0 +1,38 @@
+#---> Dictionaries and Sets
+
+# dictt={
+#     "name":"Omkar",
+#     "cgpa":7.39,
+#     "college":"RVCE",
+#     "learning":["Python","HTML","SQL","TABLEAU"],
+#     "KEY-SKILLS":("Communication","Hard Work","Smart Work")
+# } 
+# print(dictt)
+# print(type(dictt))
+# print(dictt["KEY-SKILLS"])
+# print(dictt["learning"])
+
+# Nested Dictionary
+
+student={
+    "name":"Omkar Babu Mastamardi",
+    "Marks":{
+        "MLOPS":25,
+        "DBMS":22,
+        "ANN":0,
+        "POME":24,
+        "AISE":27
+    }
+}
+
+# print(student["Marks"]["MLOPS"])
+
+# print(list(student.keys()))
+
+# print(len(student))
+# print(list(student.values()))
+# pairs=list(student.items())
+
+# print(pairs[0])
+
+#----> Sets
